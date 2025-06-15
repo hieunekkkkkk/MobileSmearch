89354fc1a1cae69bfd3d677b96d3388b2d9d5348
hieunekkkkkk <hieutmhe180168@fpt.edu.vn>
2025-06-16 00:33:31 +0700

    updatefinal

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const connectDB = require('./db');
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+
 
 connectDB();
 
-app.listen(PORT,  () => {
-    console.log(`🚀 Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
